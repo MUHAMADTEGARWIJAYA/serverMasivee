1dda8c665297d1b376286bd145e60aaeae95a7a5
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2024-12-12 00:39:57 +0700

    coba vercel

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,14 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hijaumandala"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
@@ -16,3 +20,4 @@ db.connect((err) => {
 });
 
 export default db;
+
