38ec3953d56ae2a6be4b0dd09e938c56a0aa1738
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2024-12-12 01:11:10 +0700

    config modif

--- config/Database.js
+++ config/Database.js
@@ -4,20 +4,25 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-const db = mysql.createConnection({
+// Membuat pool koneksi
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true, // Tunggu jika semua koneksi sedang digunakan
+  connectionLimit: 10,      // Maksimum koneksi dalam pool
+  queueLimit: 0,            // Antrian tidak terbatas jika koneksi penuh
 });
 
-db.connect((err) => {
+// Test koneksi pool
+db.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to the database:', err.message);
   } else {
     console.log('Connected to the MySQL database.');
+    connection.release(); // Kembalikan koneksi ke pool
   }
 });
 
 export default db;
-
