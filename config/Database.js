91d58c47585ea977c64126a9762a361e79498853
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2024-12-12 02:34:15 +0700

    modifi config

--- config/Database.js
+++ config/Database.js
@@ -1,18 +1,42 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "myuser",
-  password: "myuser",
-  database: "hijaumandala"
+// Memuat variabel lingkungan dari file .env
+dotenv.config();
+
+// Membuat koneksi pool ke database
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.message);
-  } else {
-    console.log('Connected to the MySQL database.');
+    console.error('Database connection error:', err);
+    return;
   }
+  console.log('Database terhubung....');
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection error:', err);
+    return;
+  }
+  console.log('Database terhubung....');
 });
 
 export default db;
